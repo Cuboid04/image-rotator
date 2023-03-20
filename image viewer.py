6f173e49ab0f315caddb0937de58e2d435612107
Cuboid <107613502+Cuboid04@users.noreply.github.com>
2023-03-20 20:53:31 +0530

    Add files via upload

--- image viewer.py
+++ image viewer.py
@@ -8,19 +8,27 @@ root.title("Image Viewer")
 root.configure(background="black")
 
 lbl=Label(root, highlightthickness=5, borderwidth=2, relief=SOLID, bg="yellow")
-lbl.place(relx=0.5, rely=0.5, anchor=CENTER)
+lbl.place(relx=0.5, rely=0.6, anchor=CENTER)
 img_path=""
 
 def openimage():
     global img_path
     img_path=filedialog.askopenfilename(title="Open Image File", filetypes=[("Image Files", "*.jpg *.gif *.png *.jpeg")])
     print(img_path)
-    img=Image.open(img_path)
-    img_e=ImageTk.PhotoImage(img)
+    img1=Image.open(img_path)
+    img_e=ImageTk.PhotoImage(img1)
     lbl["image"]=img_e
     img_e.close()
-    
-btn=Button(root, text="Open Image", command=openimage, font=("Lucida Sans Unicode", 20, "bold"), bg="gray")
-btn.place(relx=0.5, rely=0.1, anchor=CENTER)
 
+def rotate_img():
+    global img_path
+    im=Image.open(img_path)
+    img=ImageTk.PhotoImage(im.rotate(180))
+    lbl["image"]=img
+    img.close()
+
+btn_rotate=Button(root, text="Rotate Image", command=rotate_img, font=("Lucida Sans Unicode", 20, "bold"), bg="gray")    
+btn_open=Button(root, text="Open Image", command=openimage, font=("Lucida Sans Unicode", 20, "bold"), bg="gray")
+btn_open.place(relx=0.5, rely=0.1, anchor=CENTER)
+btn_rotate.place(relx=0.5, rely=0.3, anchor=CENTER)
 root.mainloop()
